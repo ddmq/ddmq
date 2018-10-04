b9a8ab35b5f5cf786bdf976daea62ba9243c1e14
dahlo <m.dahlo@gmail.com>
2018-10-04 13:47:06 +0200

    Config file stuff

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',  
+    'sphinx.ext.napoleon',  
 ]
 
 # Add any paths that contain templates here, relative to this directory.
